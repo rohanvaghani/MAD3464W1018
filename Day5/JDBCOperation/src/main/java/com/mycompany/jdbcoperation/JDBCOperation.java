3187891bfbfbe738c29479631f1962fc26fdfa1b
Rohan Vaghani <35268311+rohanvaghani@users.noreply.github.com>
2018-03-19 11:40:07 -0400

    Add files via upload

---
+++ Day5/JDBCOperation/src/main/java/com/mycompany/jdbcoperation/JDBCOperation.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jdbcoperation;
+
+import java.sql.*;
+
+/**
+ *
+ * @author 728464
+ */
+public class JDBCOperation {
+    static Connection conn;
+    static ResultSet rs;
+    static String USER = "root";
+    static String PASS = "";
+    static PreparedStatement stmt;
+    
+    public static void main(String[] args) {
+        connectDB();
+        selectDB();
+        insertDB();
+        deleteDB();
+    }
+    
+    static void connectDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/madwinter2018", USER, PASS);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    static void selectDB() {
+        try {
+            stmt = conn.prepareStatement("SELECT * FROM Person");
+            //stmt.execute();
+            rs = stmt.executeQuery();
+            
+            while(rs.next()) {
+                System.out.println(" ID : " + rs.getInt(1) + "   First Name : " + rs.getString("firstname") + "     Last Name : " + rs.getString("lastname") + "     Age : " + rs.getInt("age"));
+            }
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    static void insertDB() {
+        try {
+            stmt = conn.prepareStatement("insert into Person values(?,?,?,?)");
+            stmt.setInt(1, 103);
+            stmt.setString(2, "Abhishek");
+            stmt.setString(3, "Patel");
+            stmt.setInt(4, 21);
+            
+            int i = stmt.executeUpdate();
+            System.out.println(i +" recors inserted");
+       }catch(SQLException se) {
+            se.printStackTrace();
+        }
+    }
+    
+    static void deleteDB() {
+        try {
+            stmt = conn.prepareStatement("delete from Person where id = ?");
+            stmt.setInt(1, 103);
+            
+            int nrec = stmt.executeUpdate();
+            System.out.println(nrec + " records deleted");
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
